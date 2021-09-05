9effa63d08e111025335a39582bb0d43efa6f911
Marcin Tomkiewicz <marc.tomk@gmail.com>
2021-09-05 13:12:49 +0200

    Magnet Lock React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [lockStatus, setLockStatus] = useState("Geschlossen");
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (lockStatus === "Geschlossen") {
+      setLockStatus("Geöffnet")
+      setTimeLeft(3);
+      setTimeout(() => {
+        setLockStatus("Geschlossen")
+      }, 3000)
+    }
+  }
+
+  useEffect(() => {
+    if (timeLeft > 0) {
+      setTimeout(() => {
+        setTimeLeft(timeLeft-1)
+      }, 1000)
+    }
+  }, [timeLeft])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="lock__status">Der Magnetschloss ist: <span className={lockStatus === "Geschlossen" ? "locked" : "opened"}>{lockStatus}</span></div>
+      <div>Verbleibende Zeit bis zum Schließen: <span className={timeLeft > 0 ? "timer" : "timer__off"}>{timeLeft > 0 ? timeLeft : "Geschlossen"}</span></div>
+      <button onClick={handleClick}>Öffnen</button>
     </div>
   );
 }
